e7f4652ace3f54584b923f2b2c4f9b2ec01bbd89
Anoof Chappangathil <anoof7262@gmail.com>
2024-07-17 13:41:11 +0400

    BLE_CAR

--- src/master.cpp
+++ src/master.cpp
@@ -1,8 +1,17 @@
-#define RIGHT1  27
-#define RIGHT2  26
-#define LEFT1   25
-#define LEFT2   33
 
+#define RIGHT_PWM_R  22
+#define RIGHT_PWM_L  23
+#define LEFT_PWM_R   5
+#define LEFT_PWM_L   17
+
+#define RIGHT_EN_R  27   
+#define RIGHT_EN_L  26
+#define LEFT_EN_R  19
+#define LEFT_EN_L  18
+
+
+#define DIR_R  25
+#define DIR_L  33
 
 #include <Arduino.h>
 #include "BluetoothSerial.h"
@@ -10,73 +19,148 @@
 
 BluetoothSerial SerialBT;
 
-void backward(){
-    digitalWrite(RIGHT1, HIGH);
-    digitalWrite(RIGHT2, LOW);
-    digitalWrite(LEFT1, HIGH);
-    digitalWrite(LEFT2, LOW);
-}
 void forward(){
-    digitalWrite(RIGHT1, LOW);
-    digitalWrite(RIGHT2, HIGH);
-    digitalWrite(LEFT1, LOW);
-    digitalWrite(LEFT2, HIGH);
+    for (int i = 0; i < 150; i++){
+        // digitalWrite(RIGHT_EN_R, HIGH);
+        // digitalWrite(LEFT_EN_R, HIGH);
+        // digitalWrite(RIGHT_EN_L, LOW);
+        // digitalWrite(LEFT_EN_L, LOW);
+
+        analogWrite(RIGHT_PWM_R, i);
+        analogWrite(RIGHT_PWM_L, 0);
+        analogWrite(LEFT_PWM_R, i);
+        analogWrite(LEFT_PWM_L, 0);
+        delay(4);
+    }
+}
+void backward(){
+    for (int i = 0; i < 150; i++){
+        // digitalWrite(RIGHT_EN_R, LOW);
+        // digitalWrite(RIGHT_EN_L, HIGH);
+        // digitalWrite(LEFT_EN_R, LOW);
+        // digitalWrite(LEFT_EN_L, HIGH);
+
+        analogWrite(RIGHT_PWM_R, 0);
+        analogWrite(RIGHT_PWM_L, i);
+        analogWrite(LEFT_PWM_R, 0);
+        analogWrite(LEFT_PWM_L, i);
+        delay(4);
+    }
 }
 
 void right(){
-    digitalWrite(RIGHT1, HIGH);
-    digitalWrite(RIGHT2, LOW);
-    digitalWrite(LEFT1, LOW);
-    digitalWrite(LEFT2, HIGH);
+  analogWrite(DIR_R, 0);
+  analogWrite(DIR_L, 170);  
 }
 
 void left(){
-    digitalWrite(RIGHT1, LOW);
-    digitalWrite(RIGHT2, HIGH);
-    digitalWrite(LEFT1, HIGH);
-    digitalWrite(LEFT2, LOW);
+    analogWrite(DIR_R, 170);
+  analogWrite(DIR_L, 0);  
 }
-void stop(){
-    digitalWrite(RIGHT1, LOW);
-    digitalWrite(RIGHT2, LOW);
-    digitalWrite(LEFT1, LOW);
-    digitalWrite(LEFT2, LOW);
+void stop_backward(){
+    for (int i = 150; i >0; i--){
+        // digitalWrite(RIGHT_EN_R, LOW);
+        // digitalWrite(RIGHT_EN_L, HIGH);
+        // digitalWrite(LEFT_EN_R, LOW);
+        // digitalWrite(LEFT_EN_L, HIGH);
+        
+        analogWrite(RIGHT_PWM_R, 0);
+        analogWrite(RIGHT_PWM_L, i);
+        analogWrite(LEFT_PWM_R, 0);
+        analogWrite(LEFT_PWM_L, i);
+        delay(4);
+    }
+    
+}
+void stop_forward(){
+    for (int i = 150; i >0; i--){
+        // digitalWrite(RIGHT_EN_R, HIGH);
+        // digitalWrite(LEFT_EN_R, HIGH);
+        // digitalWrite(RIGHT_EN_L, LOW);
+        // digitalWrite(LEFT_EN_L, LOW);
+        
+        analogWrite(RIGHT_PWM_R, i);
+        analogWrite(RIGHT_PWM_L, 0);
+        analogWrite(LEFT_PWM_R, i);
+        analogWrite(LEFT_PWM_L, 0);
+        delay(4);
+    }
+    
+}
+void stop_left(){
+  analogWrite(DIR_R, 0);
+  analogWrite(DIR_L, 0);  
+    
+}   
+void stop_right(){
+    analogWrite(DIR_R, 0);
+  analogWrite(DIR_L, 0);  
+    
 }
 
 
 void setup()
 {
     Serial.begin(9600);
-    pinMode(RIGHT1, OUTPUT);
-    pinMode(RIGHT2, OUTPUT);
-    pinMode(LEFT1, OUTPUT);
-    pinMode(LEFT2, OUTPUT);
+    pinMode(RIGHT_PWM_R, OUTPUT);
+    pinMode(RIGHT_PWM_L, OUTPUT);
+    pinMode(LEFT_PWM_R, OUTPUT);
+    pinMode(LEFT_PWM_L, OUTPUT);
+
+    pinMode(RIGHT_EN_R, OUTPUT);
+    pinMode(RIGHT_EN_L, OUTPUT);
+    pinMode(LEFT_EN_R, OUTPUT);
+    pinMode(LEFT_EN_L, OUTPUT);
+
+    pinMode(DIR_R, OUTPUT);
+    pinMode(DIR_L, OUTPUT);
+
+    digitalWrite(RIGHT_EN_R, HIGH);
+    digitalWrite(RIGHT_EN_L, HIGH);
+    digitalWrite(LEFT_EN_R, HIGH);
+    digitalWrite(LEFT_EN_L, HIGH);
 
     SerialBT.begin("ESP32_CAR"); // Bluetooth device name
 }
 
+void loop()
+{
 
-
-void loop(){
-
-    if (SerialBT.available()){
+    if (SerialBT.available())
+    {
         char data = SerialBT.read();
         Serial.println(data);
-        if (data  == 'F'){
+        if (data == 'F')
+        {
             forward();
         }
-         if (data == 'B'){
+        if (data == 'B')
+        {
             backward();
-        } 
-         if (data == 'L'){
+        }
+        if (data == 'L')
+        {
             left();
         }
-         if (data == 'R'){
+        if (data == 'R')
+        {
             right();
         }
-         if (data == 'S'){
-            stop();
+        if (data == 'W')
+        {
+            stop_forward();
+        }
+        if (data == 'X')
+        {
+            stop_backward();
+        }
+        if (data == 'Y')
+        {
+            stop_right();
+        }
+        if (data == 'Z')
+        {
+            stop_left();
         }
     }
-
 }
